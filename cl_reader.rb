081c1497eeab0ad011997746da94f0fdd0848dd6
ealvaro <alvaroescobaronline@gmail.com>
2021-01-21 09:55:28 -0500

    4. Print each item in the format Date - Title

--- cl_reader.rb
+++ cl_reader.rb
@@ -12,6 +12,28 @@ class Scraper
     @parse_page ||= Nokogiri::HTML(doc)
   end
 
+  def get_dates
+    item_container.css(".result-date").children.map { |d| d.text }.compact
+  end
+
+  def get_titles
+    item_container.css(".result-heading").css(".result-title").children.map { |t| t.text }.compact
+  end
+
+private
+
+  def item_container
+    parse_page.css(".result-info")   # The common HTML element for the page results
+  end
+
   scraper = Scraper.new
-  puts scraper.parse_page
+  # puts scraper.parse_page
+  dates = scraper.get_dates
+  titles = scraper.get_titles
+
+  # OUTPUT
+  (0...titles.size).each do |i|
+    # puts "#{i} - "
+    puts "#{dates[i]} - #{titles[i]}"
+  end
 end
